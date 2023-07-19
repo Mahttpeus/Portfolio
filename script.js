8ddc15110c1130bc0ed4933eb4c9ffc6063e0d17
Matheus M Barbosa <73943781+Mahttpeus@users.noreply.github.com>
2023-07-19 13:55:45 -0300

    Menu responsivo + JS

---
+++ script.js
@@ -0,0 +1,7 @@
+﻿const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('cabecalho__nav__links')[0]
+
+
+toggleButton.addEventListener('click', () =>{
+    navbarLinks.classList.toggle('active')
+})
